fdfb770f219f7811ef89523c18d30c421a22e924
jstega13 <jcstegall01@gmail.com>
2025-12-11 10:41:13 -0500

    Fix pkg_resources error - upgrade setuptools in build script

--- build.sh
+++ build.sh
@@ -2,8 +2,17 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip and setuptools first
+pip install --upgrade pip setuptools wheel
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Change to Django project directory
 cd scheduling_app
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
